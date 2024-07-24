8105ffdb2de0478efc2aa8435d24b0b5721ab845
Julian Sanchez <86807205+JulianSanchez1@users.noreply.github.com>
2024-07-24 19:32:07 +0000

    second

--- src/app.py
+++ src/app.py
@@ -1,29 +1,40 @@
 import os
+import sys
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# load the .env file variables
+
+
 load_dotenv()
 
-CLIENT_ID = 'b389b433000a4c8db1129a3ea1460a50'
-CLIENT_SECERT = '9e164c0d888d4d1babe450ff57df7778'
+CLIENT_ID='b389b433000a4c8db1129a3ea1460a50'
+CLIENT_SECRET='9e164c0d888d4d1babe450ff57df7778'
 
-client_id = os.environ.get("CLIENT_ID")
-client_secert = os.environ.get("CLIENT_SECERT")
-client_credentials_manager = SpotifyClientCredentials(client_id, client_secert)
-connection = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-print(connection)
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-result = sp.artist_top_tracks("")
+if not CLIENT_ID or not CLIENT_SECRET:
+    raise ValueError("CLIENT_ID and CLIENT_SECRET must be set in the .env file")
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+print(sp)
+
+
+artist_id = "4tZwfgrHOc3mvqYlEYSvVi" 
+result = sp.artist_top_tracks(artist_id)
+
 print(type(result["tracks"]))
-tracks - result["track"][0:10]
+tracks = result["tracks"][0:10]
 print(tracks)
-sub_tracks = [(d["song"],d["popularity"], d["duration"]) for d in tracks]
 
-df = pd.DataFrame(sub_tracks)
+sub_tracks = [(d["name"], d["popularity"], d["duration_ms"]) for d in tracks]
+
+df = pd.DataFrame(sub_tracks, columns=["Song", "Popularity", "Duration"])
 print(df)
